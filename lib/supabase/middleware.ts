ef7d0165d51f02b6249431648d81ca6468ecb386
orangescrypto-jpg <orangescrypto@gmail.com>
2026-06-27 17:13:11 +0100

    Update middleware.ts

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -15,7 +15,7 @@ export function createMiddlewareClient(request: NextRequest) {
         getAll() {
           return request.cookies.getAll()
         },
-        setAll(cookiesToSet) {
+        setAll(cookiesToSet: { name: string; value: string; options: any }[]) {
           cookiesToSet.forEach(({ name, value }) =>
             request.cookies.set(name, value),
           )
