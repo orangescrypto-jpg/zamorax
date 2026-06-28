aea88e1ae571ac46e7bd37eecb3bf022e7ac8a2b
orangescrypto-jpg <orangescrypto@gmail.com>
2026-06-28 18:18:11 +0100

    Create open-next.config.ts

---
+++ open-next.config.ts
@@ -0,0 +1,7 @@
+// open-next.config.ts
+// Required config file for @opennextjs/cloudflare builds.
+// See: https://opennext.js.org/cloudflare/get-started
+
+import { defineCloudflareConfig } from "@opennextjs/cloudflare"
+
+export default defineCloudflareConfig()
