79374a5646706b0c4fbfecfc244ea082d8a45a56
orangescrypto-jpg <orangescrypto@gmail.com>
2026-06-28 20:46:15 +0000

    chore: remove Cloudflare hosting, clean unused deps

--- open-next.config.ts
+++
@@ -1,7 +0,0 @@
-// open-next.config.ts
-// Required config file for @opennextjs/cloudflare builds.
-// See: https://opennext.js.org/cloudflare/get-started
-
-import { defineCloudflareConfig } from "@opennextjs/cloudflare"
-
-export default defineCloudflareConfig()
