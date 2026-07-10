ff32d8fa446665d6f6b9e58e05f9aa26997f40d4
orangescrypto-jpg <orangescrypto@gmail.com>
2026-07-10 15:54:33 +0100

    Update page.tsx

--- app/(public)/privacy/page.tsx
+++ app/(public)/privacy/page.tsx
@@ -37,7 +37,7 @@ const DEFAULT_HTML = `
   </section>
   <section class="space-y-4">
     <h2 class="text-xl font-semibold">6. Contact</h2>
-    <p>Data Controller: Zamorax Technologies Limited, Lagos, Nigeria. Email: privacy@zamorax.ng</p>
+    <p>Data Controller: Zamorax Enterprises Limited, Ibadan, Nigeria. Email: privacy@zamorax.com</p>
   </section>
 </div>
 `
