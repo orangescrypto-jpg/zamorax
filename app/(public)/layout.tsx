0195dbfa2422fd0bf2e6b6417b661fb30f416ddc
orangescrypto-jpg <orangescrypto@gmail.com>
2026-07-11 06:04:52 +0100

    Update layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -1,6 +1,7 @@
 import { Navbar } from "@/components/layout/Navbar"
 import { Footer } from "@/components/layout/Footer"
 import { BottomNav } from "@/components/layout/BottomNav"
+import { FooterBanner } from "@/components/shared/FooterBanner"
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -9,6 +10,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
       <main className="flex-1 pt-16">
         {children}
       </main>
+      <FooterBanner />
       <Footer />
       <BottomNav />
     </>
