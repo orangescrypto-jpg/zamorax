8afde6d9c50f026944c373c0ef913f3d31b3a4cf
orangescrypto-jpg <orangescrypto@gmail.com>
2026-07-21 20:52:21 +0100

    Update layout.tsx

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -2,6 +2,7 @@ import { Navbar } from "@/components/layout/Navbar"
 import { Footer } from "@/components/layout/Footer"
 import { BottomNav } from "@/components/layout/BottomNav"
 import { FooterBanner } from "@/components/shared/FooterBanner"
+import { CartAbandonmentReminder } from "@/components/cart/CartAbandonmentReminder"
 
 export default function PublicLayout({ children }: { children: React.ReactNode }) {
   return (
@@ -13,6 +14,7 @@ export default function PublicLayout({ children }: { children: React.ReactNode }
       <FooterBanner />
       <Footer />
       <BottomNav />
+      <CartAbandonmentReminder />
     </>
   )
 }
